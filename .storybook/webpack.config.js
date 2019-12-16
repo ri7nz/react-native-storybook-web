ef6e8b4c1340e6d9f6e5090b1c7350ff71a9168e
ri7nz <ri7nz.labs@gmail.com>
2019-12-16 23:26:00 +0700

    chore(storybook): migration to version 5.x

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,7 +1,9 @@
 // https://github.com/storybooks/storybook/issues/1946
 const path = require("path");
 
-module.exports = (config, configType) => {
+module.exports = ({
+config, mode: configType
+}) => {
   config.resolve = {
     modules: ["node_modules"],
     extensions: [".web.js", ".js", ".json", ".web.jsx", ".jsx"],
